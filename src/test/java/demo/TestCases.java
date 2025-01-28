22b0fc7928899545a47361146eb13cedb5fc0442
karthik jeenu <karthikjeenu@gmail.com>
2025-01-28 08:51:37 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,10 +11,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -21,18 +20,44 @@ import demo.wrappers.Wrappers;
 public class TestCases {
     ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+    @Test
+    public void testCase01() throws InterruptedException {
+        int count = 0;
+         driver.get("https://www.flipkart.com/");
+        By search_box = By.xpath("//input[@class='Pke_EE']");
+        Wrappers.Search_text(driver,search_box, "Washing Machine");
+        WebElement populatity_filter = driver.findElement(By.xpath("//div[text()='Popularity']"));
+         populatity_filter.click();
+         Thread.sleep(3000);
+         List<WebElement> ratings=driver.findElements(By.xpath("//span[contains(@id,'productRating')]//div[text()<4.2]"));
+        count=ratings.size();
+        System.out.println("The Count of items with rating less than or equal to 4 stars are: " + count);
+    }
 
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
+    @Test
+    public void testCase02() throws InterruptedException {
+        By search_box = By.className("zDPmFV");
+        Wrappers.Search_text(driver,search_box, "iPhone");
+       Thread.sleep(2000);
+    WebElement locator=driver.findElement(By.xpath("//a[@class='CGtC98']"));
+    Wrappers.discountedIphones(locator);
+    }
+
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        By search_box = By.className("zDPmFV");
+        Wrappers.Search_text(driver,search_box, "Coffee Mug");
+       Thread.sleep(2000);
+    WebElement Four_star_above=driver.findElement(By.xpath("//div[contains(text(),'4') and contains(text(),'above')]"));
+    Wrappers.ClickonElement(driver,Four_star_above);
+    Thread.sleep(2000);
+    By locator=By.className("Wphh3N");
+    Wrappers.Top_Coffemugs(driver,locator);
+    }
+
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,7 +71,7 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
@@ -54,8 +79,7 @@ public class TestCases {
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,22 +1,126 @@
 package demo.wrappers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.NumberFormat;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    
+    private static final Logger logger = Logger.getLogger(Wrappers.class.getName());
+
+
+    // Method to search for text in a search box
+    public static void Search_text(WebDriver driver, By element, String text) {
+        try {
+            WebElement searchBox = driver.findElement(element);
+            String currentValue = searchBox.getAttribute("value");
+            if (currentValue != null && !currentValue.isEmpty()) {
+                // Send backspace to delete all characters
+                for (int i = 0; i < currentValue.length(); i++) {
+                    searchBox.sendKeys(Keys.BACK_SPACE);
+                }
+            }
+            searchBox.sendKeys(text);
+            searchBox.sendKeys(Keys.ENTER);
+            logger.info("Search performed with text: " + text);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred while searching: " + e.getMessage(), e);
+        }
+    }
+
+    public static void ClickonElement(WebDriver driver, WebElement element) {
+    if (element != null) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Ensure wait is initialized
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            element.click();
+            logger.info("Clicked on the element successfully.");
+        } catch (NoSuchElementException e) {
+            logger.log(Level.SEVERE, "Element not found: " + e.getMessage(), e);
+        } catch (StaleElementReferenceException e) {
+            logger.log(Level.SEVERE, "Element is stale: " + e.getMessage(), e);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred while clicking the element: " + e.getMessage(), e);
+        }
+    } else {
+        logger.warning("The WebElement is null. Cannot perform click action.");
+    }
 }
+
+
+    // Method to display discounted iPhones greater than 17%
+    public static void discountedIphones(WebElement locator) {
+        try {
+            List<WebElement> discounts = locator.findElements(By.xpath("//div[@class='UkUFwK']"));
+            for (WebElement discount : discounts) {
+                int percentage = Integer.parseInt(discount.getText().replaceAll("\\D", ""));
+                if (percentage > 17) {
+                    String title = locator.findElement(By.xpath("//div[@class='KzDlHZ']")).getText();
+                    logger.info("The title is: " + title + " and discount percent is: " + percentage + "%");
+                }
+            }
+        } catch (NumberFormatException e) {
+            logger.log(Level.SEVERE, "Failed to parse discount value: " + e.getMessage(), e);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An error occurred while processing discounted iPhones: " + e.getMessage(), e);
+        }
+    }
+// Method to display the top-rated coffee mugs based on rating
+public static void Top_Coffemugs(WebDriver driver,By locator) {
+    try {
+        
+        List<WebElement> ratings = driver.findElements(locator);
+        Set<Integer> uniqueRatings = new HashSet<>();
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+
+        for(WebElement rating:ratings){
+           int Userreview=Integer.parseInt(rating.getText().replaceAll("[^\\d]", ""));
+            uniqueRatings.add(Userreview);
+        }
+        
+
+        // Sorting ratings in descending order
+        List<Integer> sortedRatings = new ArrayList<>(uniqueRatings);
+        Collections.sort(sortedRatings, Collections.reverseOrder());
+
+        // Displaying the top 5 coffee mugs or available ratings
+        int topRatingsCount = Math.min(sortedRatings.size(), 5);
+        for (int i = 0; i < topRatingsCount; i++) {
+            String number = "(" + numberFormat.format(sortedRatings.get(i)) + ")";
+
+            // Wait for title and image elements to appear
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement titleElement = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//span[text()='" + number + "']/../../a[2]")));
+            WebElement imageElement = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//span[text()='" + number + "']/../../a[1]//img")));
+
+            // Retrieve the title and image URL
+            String title = titleElement.getText();
+            String productImage = imageElement.getAttribute("src");
+
+            // Log the title and image
+            logger.info((i + 1) + ". The title is " + title + " and image URL is " + productImage);
+        }
+    } catch (Exception e) {
+        logger.log(Level.SEVERE, "An error occurred while processing top coffee mugs: " + e.getMessage(), e);
+    }
+}
+
+  }
